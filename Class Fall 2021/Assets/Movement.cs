404d3136bf8b9c0aed7a80ee048c2ad0f1ffa38b
dkletenik <kletenik@sci.brooklyn.cuny.edu>
2021-10-06 09:58:47 -0400

    changes made in class October 6; added movement script to Mario to allow him to move right & left and jump

---
+++ Class Fall 2021/Assets/Movement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float movement;
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] bool isFacingRight = true;
+    [SerializeField] bool jumpPressed = false;
+    [SerializeField] float jumpForce = 500.0f;
+    [SerializeField] bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rigid == null)
+            rigid = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame; best for user input
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+            jumpPressed = true;
+    }
+
+    //FixedUpdate is called potentially multiple times per frame; best for physics and movement
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(movement * speed, rigid.velocity.y);
+        if (movement < 0 && isFacingRight || movement > 0 && !isFacingRight)
+            Flip();
+
+        if (jumpPressed && isGrounded)
+            Jump();
+    }
+
+    private void Flip()
+    {
+        //Vector3 playerScale = transform.localScale;
+        //playerScale.x = playerScale.x * -1;
+        //transform.localScale = playerScale;
+
+        transform.Rotate(0, 180, 0);
+        isFacingRight = !isFacingRight;
+
+    }
+
+    private void Jump()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, 0);
+        rigid.AddForce(new Vector2(0, jumpForce));
+        jumpPressed = false;
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+
+}
