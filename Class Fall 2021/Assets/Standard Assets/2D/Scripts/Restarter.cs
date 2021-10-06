f0e6be5dd6da55a9794080df7668437c93b7ecb8
dkletenik <kletenik@sci.brooklyn.cuny.edu>
2021-10-05 22:56:56 -0400

    initial setup, lecture of Oct 4

---
+++ Class Fall 2021/Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+    public class Restarter : MonoBehaviour
+    {
+        [SerializeField] int score = 0;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+            }
+
+            if (other.tag == "Crate")
+            {
+                score++;
+                Debug.Log("score: " + score);
+            }
+        }
+    }
+}
