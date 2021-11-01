8d2070c65c8884bf5a8d552261afe278b96538ed
dkletenik <kletenik@sci.brooklyn.cuny.edu>
2021-11-01 10:11:38 -0400

    Nov 1, Unity lecture #7, sprite sheets and animations

--- Class Fall 2021/Assets/Movement.cs
+++ Class Fall 2021/Assets/Movement.cs
@@ -11,13 +11,21 @@ public class Movement : MonoBehaviour
     [SerializeField] bool jumpPressed = false;
     [SerializeField] float jumpForce = 500.0f;
     [SerializeField] bool isGrounded = true;
+    [SerializeField] Animator anim;
 
+    const int IDLE = 0;
+    const int RUN = 1;
+    const int JUMP = 2;
+ 
     // Start is called before the first frame update
     void Start()
     {
         if (rigid == null)
             rigid = GetComponent<Rigidbody2D>();
-        
+        if (anim == null)
+            anim = GetComponent<Animator>();
+        anim.SetInteger("motion", IDLE);
+   
     }
 
     // Update is called once per frame; best for user input
@@ -31,12 +39,23 @@ public class Movement : MonoBehaviour
     //FixedUpdate is called potentially multiple times per frame; best for physics and movement
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(movement * speed, rigid.velocity.y);
-        if (movement < 0 && isFacingRight || movement > 0 && !isFacingRight)
-            Flip();
+        if (isGrounded)
+        {
+            rigid.velocity = new Vector2(movement * speed, rigid.velocity.y);
+            if (movement > .01 || movement < -.01)
+            {
+                anim.SetInteger("motion", RUN);
+            }
+            else
+            {
+                anim.SetInteger("motion", IDLE);
+            }
+            if (movement < 0 && isFacingRight || movement > 0 && !isFacingRight)
+                Flip();
 
-        if (jumpPressed && isGrounded) //only allow Mario to jump if he is on the ground -- no double jumps 
-            Jump();
+            if (jumpPressed && isGrounded) //only allow Mario to jump if he is on the ground -- no double jumps 
+                Jump();
+        }
     }
 
     private void Flip()
@@ -55,7 +74,8 @@ public class Movement : MonoBehaviour
         rigid.velocity = new Vector2(rigid.velocity.x, 0);
         rigid.AddForce(new Vector2(0, jumpForce));
         jumpPressed = false;
-        isGrounded = false; 
+        isGrounded = false;
+        anim.SetInteger("motion", JUMP);
     }
 
     //when we collide with the ground (here our ramp), we want to note that Mario is grounded
